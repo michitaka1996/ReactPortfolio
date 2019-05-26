21e93e4500014a45f0ac48f40f9237887ac5f353
michitaka <michirug11@i.softbank.jp>
2019-05-26 18:18:53 +0900

    TOPページのコーディング

---
+++ sh/gulp.sh
@@ -0,0 +1,13 @@
+#!/bin/shs
+
+
+npm install browserify -D
+npm install gulp-cli -g
+npm install gulp -D
+#cssを圧縮するもの
+npm install gulp-minify-css -D
+#sass型式をcssに変換する
+npm install gulp-sass -D 
+#画像を圧縮するもの
+npm install gulp-imagemin -D
+npm install gulp-changed -D
